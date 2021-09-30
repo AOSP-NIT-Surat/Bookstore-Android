c3935690df39f14373b8a53b13cd1c9e0a1867df
Mustafa3011 <officewalamustafa@gmail.com>
2021-09-30 21:04:09 +0530

    basic: Initialized Repo Structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bookstore-Android
 Android application for Bookstore, based on Java
+## Tech Stack
+- Java
+- Gradle to build
+- Xml for ui
---
+++ app/src/main/java/com/aosp/bookstore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aosp.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO : Create a login page
+        setContentView(R.layout.activity_main);
+    }
+}
