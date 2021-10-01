f1e4ee3810512b3b33b596ee601cfbaf7a496aa1
mayorjay <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>
2021-10-02 00:19:04 +0100

    feat: Create a login page

--- app/src/main/java/com/aosp/bookstore/MainActivity.java
+++ app/src/main/java/com/aosp/bookstore/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO : Create a login page
         setContentView(R.layout.activity_main);
     }
 }
