1e81914f00ffac8402e6c70b75743f79da75cfd0
Joseph Olugbohunmi <37001871+mayorJAY@users.noreply.github.com>
2021-10-02 05:16:23 +0100

    Create a login page (#2)
    * feat: Create a login page
    
    * feat: Add Sign up option
    
    Authored-by: mayorjay <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>
    
    Working as intended, no changes required

---
+++ app/src/main/java/com/aosp/bookstore/login/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.aosp.bookstore.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aosp.bookstore.MainActivity;
+import com.aosp.bookstore.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText emailEditText, passwordEditText;
+    private TextInputLayout emailEditTextLayout, passwordEditTextLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.email_input_editText);
+        emailEditTextLayout = findViewById(R.id.email_input_layout);
+        passwordEditText = findViewById(R.id.password_input_editText);
+        passwordEditTextLayout = findViewById(R.id.password_input_layout);
+        TextView registerTv = findViewById(R.id.register_text);
+        MaterialButton submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(v -> performLogin());
+        registerTv.setOnClickListener(v -> navigateToSignUp());
+    }
+
+    private void performLogin() {
+        String email = String.valueOf(emailEditText.getText());
+        String password = String.valueOf(passwordEditText.getText());
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditTextLayout.setError("Invalid Email address");
+            return;
+        } else {
+            emailEditTextLayout.setError(null);
+        }
+
+        if (password.length() < 8) {
+            passwordEditTextLayout.setError("Password cannot be less than 8 characters");
+            return;
+        } else {
+            passwordEditTextLayout.setError(null);
+        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void navigateToSignUp() {
+        // Todo: Create Sign up page
+        Toast.makeText(this, R.string.register, Toast.LENGTH_SHORT).show();
+    }
+}
